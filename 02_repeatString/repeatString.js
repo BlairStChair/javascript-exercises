97c5d96c747e237081e3ff8a8cc1ca424fc02cec
BlairStChair <julia.z15@interia.pl>
2024-05-24 14:53:46 +0200

    Done the second exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word = '', number = 0) {  
+    let concated_words = '';
+    
+    if(number < 0){
+        return 'ERROR';
+    }else{
+    for(let i = 0; i < number; i++){
+            concated_words = concated_words.concat(word);
+      }
+      return concated_words;}
 };
 
 // Do not edit below this line
