2d3b99cfa5e3002eef727233305d2ed2c67dad75
BlairStChair <julia.z15@interia.pl>
2024-05-31 22:04:56 +0200

    Finished third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+const reverseString = function(word = '') {
+    let new_array = [];
+        for(let i = 0; i < word.length; i++){
+           new_array[i] = word[word.length - i - 1];
+            }
+        return new_array.join('');
+        };
 
 // Do not edit below this line
 module.exports = reverseString;
