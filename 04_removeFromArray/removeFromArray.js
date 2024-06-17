ecd486f58672c44ee7d9da375bc7b58bfa9503ff
BlairStChair <julia.z15@interia.pl>
2024-06-17 16:46:27 +0200

    Finished forth js exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(userArray = [], ...argumentsToRemove) {
+         
+    for(let i = 0; i < userArray.length; i++){
+    argumentsToRemove.forEach((argumentToRemove) => {
+     let index = userArray.indexOf(argumentToRemove);
+     if(index >= 0){
+         userArray.splice(index, 1);
+     }
+     
+    })}
+  
+  return userArray;
 };
 
 // Do not edit below this line
