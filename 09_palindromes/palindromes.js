d55f850fe1e26afa2e8aee84c79a4cd479c3c748
BlairStChair <julia.z15@interia.pl>
2024-07-15 11:09:08 +0200

    Completed this exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let lowerCaseStr = str.toLowerCase();
+    const myArray = lowerCaseStr.split("");
+    const justLetters = myArray.filter(item => item !== ' ' && item !== ',' && item !== '.' && item !== '!');
+    const reversedArray = [...justLetters].reverse();
+    if(justLetters.join('') === reversedArray.join('')){
+      return true;
+    }else{
+      return false;
+    }
 };
 
 // Do not edit below this line
