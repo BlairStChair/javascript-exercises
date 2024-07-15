6038c606a47583dbbb14b75ff96b2f7ad5daa006
BlairStChair <julia.z15@interia.pl>
2024-07-15 11:30:56 +0200

    Completed this exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(fLength) {
+    if(fLength >= 0){
+    const fArray = [1,1];
+    for(i = 1; i < fLength - 1; i++){
+      fArray[i+1] = fArray[i-1] + fArray[i];
+    }
+    if(fLength == 0){
+        return 0;
+    }else{
+    return fArray[fLength -1];
+    }
+    }else{
+        return 'OOPS';
+    }
 };
 
 // Do not edit below this line
